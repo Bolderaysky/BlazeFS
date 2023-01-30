2d7a391b7b69b5c3822506654b28578336c96413
Thomas Zuev <73255292+Bolderaysky@users.noreply.github.com>
2023-01-31 01:28:27 +0200

    add cmake function to allow easier using submodules in cmake

---
+++ cmake/AddGitSubmodule.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required(VERSION 3.15)
+
+function(add_git_submodule dir)
+
+find_package(Git REQUIRED)
+
+if(NOT EXISTS ${dir}/CMakeLists.txt)
+  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND_ERROR_IS_FATAL ANY)
+endif()
+
+add_subdirectory(${dir})
+
+endfunction(add_git_submodule)
